32b7ac8da386db05e08bc809459a96135aaef3ba
certifiedloverboy <165409545+certifiedloverboy@users.noreply.github.com>
2025-02-06 16:19:23 -0800

    d

--- api/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'hello from the backend' });
-});
-
-module.exports = app;
